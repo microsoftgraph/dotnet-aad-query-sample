81f511ca52ba3239fb8d98283f91c43740f09ba9
Luca Spolidoro <{ID}+{username}@users.noreply.github.com>
2021-02-02 11:23:27 -0800

    using new new syntax

--- MsGraphSamples.WPF/Helpers/SecretConfig.cs
+++ MsGraphSamples.WPF/Helpers/SecretConfig.cs
@@ -8,7 +8,7 @@ namespace MsGraph_Samples.Helpers
         private readonly static IConfiguration _configuration = new ConfigurationBuilder().AddUserSecrets<App>().Build();
 
         private const string _helpUrl = "https://docs.microsoft.com/aspnet/core/security/app-secrets?tabs=windows";
-        private readonly static FileFormatException _configException = new FileFormatException($"Missing or invalid secrets.json\nMake sure you created one: {_helpUrl}");
+        private readonly static FileFormatException _configException = new($"Missing or invalid secrets.json\nMake sure you created one: {_helpUrl}");
 
         public static string ClientId => _configuration["clientId"] ?? throw _configException;
     }
